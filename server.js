64288d6da4d0d5dfaffcab223cf9d68120accb90
BayronECV <bayron.carrasco@2brains.cl>
2022-04-18 22:15:12 -0400

    3.- Cambio de puerto 8080

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ const app = express();
 
 app.get('/', (req, res) => res.send('<h1>Hola mundo con express HTML</h1>'))
 
-app.listen(3000);
-console.log('Servidor en el puerto 3000')
+app.listen(8080);
+console.log('Servidor en el puerto 8080')
