7691151882351c4429987d23591a67b245e2293d
BayronECV <bayron.carrasco@2brains.cl>
2022-04-18 22:12:36 -0400

    2.- Cambio de texto

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Hola mundo con express'))
+app.get('/', (req, res) => res.send('<h1>Hola mundo con express HTML</h1>'))
 
 app.listen(3000);
 console.log('Servidor en el puerto 3000')
